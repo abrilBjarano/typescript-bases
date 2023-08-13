a9ea24ecfe1df6626ddb1ee4d13b75467fd0405c
abrilxcx <abrlbjrn@gmail.com>
2023-08-13 00:09:26 -0600

    objetos, arreglos e interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 // import './topics/01-basic-types';
+import './topics/02-object-interface';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   hola mundo
 `
 
-console.log('holas');
+// console.log('holas');
