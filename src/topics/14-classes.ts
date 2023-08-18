5ff802672bf80266691c97228e666ac41502cad8
abrilxcx <abrlbjrn@gmail.com>
2023-08-17 23:39:25 -0600

    extender una clase

--- src/topics/14-classes.ts
+++ src/topics/14-classes.ts
@@ -18,3 +18,20 @@ const grisi = new Person('Abril', 'Monterrey, NL');
 const nat = new Person('Natalia', 'Cadereyta, NL', 'O');
 console.log( grisi );
 console.log( nat );
+
+
+
+/*  EXTENDER UNA CLASE  */
+
+export class Ingeniero extends Person {
+   constructor (
+      public cedula: boolean,
+      public titulo: boolean,
+      public name: string
+   ){
+      super( name, 'Monterrey, NL', 'No hay tipo de sangre' );
+   }
+}
+
+const eduardo = new Ingeniero(true, true, 'Eduardo');
+console.log(eduardo);
