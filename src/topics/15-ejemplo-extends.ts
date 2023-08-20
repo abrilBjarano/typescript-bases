61bbdf2b7999efdd99f4bb00ac50107ffa05b8eb
abrilxcx <abrlbjrn@gmail.com>
2023-08-20 15:24:45 -0600

    clases basicas 2

--- src/topics/15-ejemplo-extends.ts
+++ src/topics/15-ejemplo-extends.ts
@@ -7,20 +7,33 @@ class Disco {
    ){}
 }
 
-const fijacionOral = new Disco('Fijación Oral', 'Shakira', 11);
-console.log( fijacionOral );
+// const fijacionOral = new Disco('Fijación Oral', 'Shakira', 11);
+// console.log( fijacionOral );
 
 
-class DiscoEspecial extends Disco {
+// class DiscoEspecial extends Disco {
+
+//    constructor(
+
+//       public name: string,
+//       public singer: string,
+//       public songs: number,
+//       public aditivo: string
+//    ){
+//       super(name, singer, songs);
+//    }
+// }
+
+class DiscoEspecial {
 
    constructor(
 
-      public name: string,
+      public name: string, 
       public singer: string,
       public songs: number,
       public aditivo: string
    ){
-      super(name, singer, songs);
+
    }
 }
 
---
+++ src/topics/16-classes2.ts
@@ -0,0 +1,13 @@
+export class Person {
+   public name: string;
+   public address: string;
+
+   constructor(){
+      this.name = 'Abril';
+      this.address = 'Monterrey';
+   }
+}
+
+const abril = new Person();
+console.log(abril);
+
